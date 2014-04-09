7e38dce74cb2d213fb07c9d86c4f9c9ebb7cc598
Abid ANIS <anab.tn@gmail.com>
2014-04-09 01:42:57 +0100

    add bg face

---
+++ assets/javascripts/script-master.js
@@ -0,0 +1,33 @@
+var tunisiana = {
+    version: '1.1.2',
+    date: '2014.jun.06',
+    debug: true,
+	currentFrame: 1,
+	consolelog: {
+        css: 'background: #FFF; color: #E3001B'
+    },
+	log: function(msg,notcustom) {
+        if (this.debug && !$.browser.msie){
+			if(!notcustom)
+	            console.log('%c LOG : ', this.consolelog.css, msg);
+			else
+				console.log(msg);
+		}
+    },
+	host: 'http://localhost:8080',
+	grid : [50 ,50],
+};
+$( function() {
+	// Display the right thumbnail instead of the slideshow animated gif when the slideshow is running
+	$( 'body' ).bind( 'vegaswalk', function( e, bg ) {
+	    var src = $( bg ).attr( 'src' ).replace( 'background', 'thumbnail' );
+		$( '#thumbnail' ).attr( 'src', src );
+
+	});
+
+	// Auto Vegaswalk
+	$( '#pause' ).trigger( "click" );
+
+
+ });
+
